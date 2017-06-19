bee38469ba01ce8d1ceef72ff91ecf37fbe88c57
ADIT-DESKTOP <aditiyak.oppo@gmail.com>
2017-06-19 15:03:27 +0700

    Init
    Init-Master

---
+++ common/config/main-local.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=oppoit_spiderman',
+            'username' => 'oppoit_spiderman',
+            'password' => 'atQ7wML84t',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
